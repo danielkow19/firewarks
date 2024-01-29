17c0ffe11939c82fd58df1cd82263a99a072c528
Elumenix <dps5393@g.rit.edu>
2024-01-29 18:34:01 -0500

    Set up basic project
    Created a main scene and a test script to move an object with the WASD keys

---
+++ TestScript.cs
@@ -0,0 +1,34 @@
+using Godot;
+using System;
+
+public partial class TestScript : MeshInstance2D
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		if (Input.IsKeyPressed(Key.W))
+		{
+			Translate(new Vector2(0.0f, -1.0f));
+		}
+		
+		if (Input.IsKeyPressed(Key.A))
+		{
+			Translate(new Vector2(-1.0f, 0.0f));
+		}
+		
+		if (Input.IsKeyPressed(Key.S))
+		{
+			Translate(new Vector2(0.0f, 1.0f));
+		}
+		
+		if (Input.IsKeyPressed(Key.D))
+		{
+			Translate(new Vector2(1.0f, 0.0f));
+		}
+	}
+}
