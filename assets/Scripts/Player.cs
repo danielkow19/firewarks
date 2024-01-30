8da952c437ebb9d869a69ab6caf11a75960844ea
Donovan Scullion (RIT Student) <dps5393@rit.edu>
2024-01-30 13:29:34 -0500

    Confirmed collision shape works via debugging

--- assets/Scripts/Player.cs
+++ assets/Scripts/Player.cs
@@ -1,12 +1,17 @@
+using System;
+using System.Diagnostics;
 using Godot;
 
 namespace FireWARks.assets.Scripts;
 
 public partial class Player : Area2D
 {
+	private CollisionShape2D _collider;
+	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		_collider = GetNode<CollisionShape2D>("%Collider");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -30,6 +35,11 @@ public partial class Player : Area2D
 		if (Input.IsKeyPressed(Key.D))
 		{
 			Translate(new Vector2(1.0f, 0.0f));
+		}
+
+		if (GetOverlappingAreas().Count != 0)
+		{
+			Debug.Print(GetOverlappingAreas().ToString());
 		}
 	}
 }
