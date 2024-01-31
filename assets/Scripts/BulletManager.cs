52a5f7dc343544eff9244bf76561a31cdec712e2
chigbe001 <chigbe001@gmail.com>
2024-01-31 14:41:15 -0500

    Bullet pattern and manger nodes created

---
+++ assets/Scripts/BulletManager.cs
@@ -0,0 +1,17 @@
+using Godot;
+using System;
+
+public partial class BulletManager : Node
+{
+	//unsure what should be handled here created as an assigned task but will discuss what/if to utilize
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+}
---
+++ assets/Scripts/Pattern.cs
@@ -0,0 +1,19 @@
+using Godot;
+using System;
+
+public partial class Pattern : Node
+{
+	//base class for a bullet pattern, returns function or path for x bullet in pattern at y time
+	//creates UI warning for bullet paths etc
+
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+}
