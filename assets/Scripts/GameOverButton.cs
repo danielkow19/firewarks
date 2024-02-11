f9e7e54ee48ff33c0e3d9081910133787df1349b
Patrick Doherty <prd9410@g.rit.edu>
2024-02-11 16:08:24 -0500

    Added in scene transitions and game over logic

---
+++ assets/Scripts/GameOverButton.cs
@@ -0,0 +1,23 @@
+using Godot;
+using System;
+
+public partial class GameOverButton : Node
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		Button button = GetNode<Button>("Button");
+		button.Pressed += OnButtonPressed;
+	}
+
+	private void OnButtonPressed()
+	{
+		GetTree().ChangeSceneToFile("res://StartMenu.tscn");
+
+    }
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+}
