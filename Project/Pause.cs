d11fd5e73c1c1eadf7a769df9ee5b88a56651aa7
Patrick Doherty <prd9410@g.rit.edu>
2024-02-21 18:15:52 -0500

    Current UI works with controllers, fixed small Pause bug

--- Project/Pause.cs
+++ Project/Pause.cs
@@ -17,6 +17,7 @@ public partial class Pause : Control
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+
 	}
 	public void _on_quit_pressed()
 	{
