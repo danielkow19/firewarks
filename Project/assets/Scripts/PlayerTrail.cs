3f0b4a21e4efec9c2ab8b0402cf43a9d3d261ef6
Donovan Scullion (RIT Student) <dps5393@rit.edu>
2024-02-22 13:48:18 -0500

    player trail tracks player

--- Project/assets/Scripts/PlayerTrail.cs
+++ Project/assets/Scripts/PlayerTrail.cs
@@ -4,26 +4,29 @@ namespace FireWARks.assets.Scripts;
 
 public partial class PlayerTrail : Line2D
 {
-    private int maxPoints = 100;
-    private Curve2D curve;
-    
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
-    {
-        curve = new Curve2D();
-    }
+	private int maxPoints = 100;
+	private Curve2D curve;
+	
+	
+		//../../Camera2D
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		curve = new Curve2D();
+	}
 
-    // Called every frame. 'delta' is the elapsed time since the previous frame.
-    public override void _Process(double delta)
-    {
-        curve.AddPoint(((Area2D)GetParent()).GetRelativeTransformToParent(GetParent().GetParent()).Origin);
-        //GD.Print( "Position: " + ((Area2D)GetParent()).GetRelativeTransformToParent(GetParent().GetParent()).Origin);
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		Area2D parent = GetParent<Area2D>();
 
-        if (curve.PointCount > maxPoints)
-        {
-            curve.RemovePoint(0);
-        }
+		curve.AddPoint(parent.Position);
 
-        Points = curve.GetBakedPoints();
-    }
+		if (curve.PointCount > maxPoints)
+		{
+			curve.RemovePoint(0);
+		}
+
+		Points = curve.GetBakedPoints();
+	}
 }
