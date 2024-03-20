894a8b060efeaaab7b5c3a16cf58d9d8d9f0835c
chigbee001 <chigbee001@gmail.com>
2024-03-20 00:45:38 -0400

    initial cloud structure implmentation

--- Project/assets/Scripts/Clouds.cs
+++ Project/assets/Scripts/Clouds.cs
@@ -1,3 +1,4 @@
+using System;
 using Godot;
 using Godot.Collections;
 
@@ -5,14 +6,32 @@ namespace FireWARks.assets.Scripts;
 
 public partial class Clouds : Area2D
 {
+	[Export(PropertyHint.Enum, "Up,Down,Left,Right,UpAndLeft,UpAndRight,DownAndLeft,DownAndRight")]
+	public string[] directions = new string[0];
+	[Export]
+	public double[] speed = new double[0];
+	[Export]
+	public double[] rotations = new double[0];
+	[Export]
+	public double[] timers = new double[0];
+	[Export]
+	public bool moving = false;
+	[Export]
+	public bool boomerang = false;
+	private int step = 0;
+	private Vector2 start;
+	private bool reverseTracking = false;
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		start = Position;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		//this.Position = this.Position + (path[step].Normalized().X * speed[step]);
 	}
 	
 	private void _on_area_entered(Area2D area)
