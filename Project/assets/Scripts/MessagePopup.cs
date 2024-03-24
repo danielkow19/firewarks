74a957f16b5a2239a9b23787b474c1621c39bc96
Donovan Scullion (RIT Student) <dps5393@rit.edu>
2024-03-24 16:15:53 -0400

    Finally got text popup working

---
+++ Project/assets/Scripts/MessagePopup.cs
@@ -0,0 +1,12 @@
+using Godot;
+using System;
+
+public partial class MessagePopup : Marker2D
+{
+	public void Popup(string message, Vector2 position)
+	{
+		GlobalPosition = position;
+		GetChild<Label>(0).Text = message;
+		GetChild<AnimationPlayer>(1).Play();
+	}
+}
