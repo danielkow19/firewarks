4236c26496420267dc25c16efbf1810498ed73b1
chigbe001 <chigbe001@gmail.com>
2024-03-26 15:25:18 -0400

    Willow balance update and player trail optimization

--- Project/assets/Scripts/PlayerTrail.cs
+++ Project/assets/Scripts/PlayerTrail.cs
@@ -1,11 +1,12 @@
 using System.Collections.Generic;
+using System.Linq;
 using Godot;
 
 namespace FireWARks.assets.Scripts;
 
 public partial class PlayerTrail : Line2D
 {
-	//private int maxPoints = 100;
+	private int maxPoints = 100;
 	private Curve2D curve;
 	private List<CollisionShape2D> tailSegments;
 	private List<float> timeActive;
@@ -14,6 +15,13 @@ public partial class PlayerTrail : Line2D
 	private bool isVisible = true;
 	private bool deleteFirstValue = true;
 
+	private Area2D segment = new Area2D();
+	private CollisionShape2D collisionShape = new CollisionShape2D();
+	private Player parent;
+	private Callable callable;
+	private List<float> lengths;
+	
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
@@ -21,58 +29,66 @@ public partial class PlayerTrail : Line2D
 		curve = new Curve2D();
 		tailSegments = new List<CollisionShape2D>();
 		timeActive = new List<float>();
+		lengths = new List<float>();
 		Modulate = playerRef.Modulate;
+		collisionShape.Shape = new SegmentShape2D();
+		segment.CollisionLayer = 2;
+		parent = GetParent<Area2D>() as Player;
+		callable = new Callable(this, nameof(OnAreaEntered));
 		
-		/*for (int i = 0; i < maxPoints - 1; i++)
+		for (int i = 0; i < maxPoints - 1; i++)
 		{
 			// Temp, doesn't need to be saved in class but will in scene
-			Area2D segment = new Area2D();
-			CollisionShape2D collisionShape = new CollisionShape2D();
-			collisionShape.Shape = new SegmentShape2D();
-			segment.AddChild(collisionShape);
-			segment.CollisionLayer = 2;
-				
-			tailSegments.Add(collisionShape);
+			Area2D newSegment = (Area2D)segment.Duplicate();
+			CollisionShape2D newColShape = (CollisionShape2D)collisionShape.Duplicate();
+			newSegment.AddChild(newColShape);	
+			tailSegments.Add(newColShape);
 			((SegmentShape2D)tailSegments[i].Shape).A = Position;
 			((SegmentShape2D)tailSegments[i].Shape).B = Position;
-			
-			// Set up signals for the area2Ds so they don't need to be saved
-			AddChild(segment);
+			AddChild(newSegment);
 			
 			// Create a Callable for the OnAreaEntered method; This allows signals
-			Callable callable = new Callable(this, nameof(OnAreaEntered));
-			segment.Connect("area_entered", callable);
-		}*/
+			newSegment.Connect("area_entered", callable);
+		}
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		/*Player parent = GetParent<Area2D>() as Player;
-		foreach (var segment in tailSegments)		
-		{
-			RemoveChild(segment);
-			segment.QueueFree();
-		}
+		
+		Points = curve.GetBakedPoints(); // TODO: will this add points? no callable function yet
 
 		// Add new point
 		curve.AddPoint(new Vector2(parent.Position.X + 20 * (Mathf.Cos(parent.Rotation) * -1), 
 			parent.Position.Y + 20 * (Mathf.Sin(parent.Rotation) * -1)));
-		timeActive.Add(0); // new time tracker
-
+		timeActive.Add(0); // new time tracker		
+		if(Points.Length > 1){
+			lengths.Add(Points.Last<Vector2>().DistanceTo(Points[Points.Length - 1]));
+			Area2D newSegment = (Area2D)segment.Duplicate();
+			CollisionShape2D newColShape = (CollisionShape2D)collisionShape.Duplicate();
+			newSegment.AddChild(newColShape);	
+			tailSegments.Add(newColShape);
+			((SegmentShape2D)tailSegments[tailSegments.Count-1].Shape).A = Points[Points.Length - 1];
+			((SegmentShape2D)tailSegments[tailSegments.Count-1].Shape).B = Points[Points.Length - 2];
+			AddChild(newSegment);
+			
+			// Create a Callable for the OnAreaEntered method; This allows signals
+			newSegment.Connect("area_entered", callable);
+		}
+		
 		// Fixes problem with player teleporting after first frame
 		if (deleteFirstValue)
 		{
 			curve.RemovePoint(0);
 			timeActive.RemoveAt(0);
+			var deleteThis = tailSegments[0];
+			tailSegments.RemoveAt(0);
+			deleteThis.QueueFree();
 			deleteFirstValue = false;
 		}
 		
 		// Only start deleting if a line is drawn
 		if (curve.PointCount <= 1) return;
-		
-		
-		float totalLength = 0;
 		
 		for (int i = 0; i < curve.PointCount; i++)
 		{
@@ -81,95 +97,35 @@ public partial class PlayerTrail : Line2D
 			{
 				timeActive[i] += (float)delta;
 			}
-
-			float saveDist = 0;
-			
-			// Get the length so far; checking first point would crash
-			if (i != 0 && i != curve.PointCount - 1)
-			{
-				saveDist = Points[i].DistanceTo(Points[i - 1]);
-			}
-			else if (i == curve.PointCount - 1)
-			{
-				// This block is for the newest point as it isn't part of curve
-				saveDist = new Vector2(parent.Position.X + 20 * (Mathf.Cos(parent.Rotation) * -1),
-					parent.Position.Y + 20 * (Mathf.Sin(parent.Rotation) * -1)).DistanceTo(Points[i - 1]);
-			}
-
-			// Due to player spawning there will occasionally be random large values at the beginning
-			if (saveDist < 10)
-			{
-				totalLength += saveDist;
-			}
 		}
 
 		#region DistanceCulling
 		
 		float goalLength = 150f / (parent.Health + 1);
-		float startCutting = totalLength - goalLength;
-
-		// Line is too long start removing points at the beginning
-		if (startCutting > 0)
+		while(lengths.Sum() > goalLength)
 		{
-			float activeLength = 0;
-			int cutIndex = -1;
-
-			for (int i = 1; i < Points.Length; i++)
-			{
-				activeLength += Points[i].DistanceTo(Points[i - 1]);
-
-				if (activeLength > startCutting)
-				{
-					cutIndex = i;
-					break;
-				}
-			}
-			
-			// It was most recent point that put it over, only remove one point
-			if (cutIndex == -1)
-			{
-				cutIndex = 1;
-			}
-
-			int count = 0;
-			while (count < cutIndex)
-			{
-				curve.RemovePoint(0);
-				timeActive.RemoveAt(0);
-				count++;
-			}
+			curve.RemovePoint(0);
+			timeActive.RemoveAt(0);
+			lengths.RemoveAt(0);
+			var deleteThis = tailSegments[0];
+			tailSegments.RemoveAt(0);
+			deleteThis.QueueFree();
 		}
+		
 		#endregion
 		
 		// Time Culling
 		float timeAllowed = 1.0f / (parent.Health + 1);
-		while (timeActive[0] > timeAllowed)
+		while(timeActive[0] > timeAllowed)
 		{
 			curve.RemovePoint(0);
 			timeActive.RemoveAt(0);
+			lengths.RemoveAt(0);
+			var deleteThis = tailSegments[0];
+			tailSegments.RemoveAt(0);
+			deleteThis.QueueFree();
 		}
 		
-		// Save Collision segments that should accurately represent the curve
-		tailSegments.Clear();
-		Points = curve.GetBakedPoints(); // TODO: will this add points? no callable function yet
-		for (int i = 0; i < Points.Length - 1; i++)
-		{
-			// Temp, doesn't need to be saved in class but will in scene
-			Area2D segment = new Area2D();
-			CollisionShape2D collisionShape = new CollisionShape2D();
-			collisionShape.Shape = new SegmentShape2D();
-			segment.AddChild(collisionShape);
-			segment.CollisionLayer = 2;
-				
-			tailSegments.Add(collisionShape);
-			((SegmentShape2D)tailSegments[i].Shape).A = Points[i];
-			((SegmentShape2D)tailSegments[i].Shape).B = Points[i + 1];
-			AddChild(segment);
-			
-			// Create a Callable for the OnAreaEntered method; This allows signals
-			Callable callable = new Callable(this, nameof(OnAreaEntered));
-			segment.Connect("area_entered", callable);
-		}*/
 	}
 	
 	private void OnAreaEntered(Area2D area)
