ed261fd24344ae51b78fa2afeffd0484989183b9
Donovan Scullion (RIT Student) <dps5393@rit.edu>
2024-03-28 13:12:14 -0400

    Removed unused variables (and second player reference) from playertrail

--- Project/assets/Scripts/PlayerTrail.cs
+++ Project/assets/Scripts/PlayerTrail.cs
@@ -9,16 +9,16 @@ public partial class PlayerTrail : Line2D
 	private Curve2D curve;
 	private List<CollisionShape2D> tailSegments;
 	private List<float> timeActive;
-	private Player playerRef;
+	//private Player playerRef;
 
 	private bool isVisible = true;
-	private bool deleteFirstValue = true;
+	//private bool deleteFirstValue = true;
 
 	private Area2D segment = new Area2D();
 	private CollisionShape2D collisionShape = new CollisionShape2D();
-	private Player parent;
+	private Player playerRef;
 	private Callable callable;
-	private List<float> lengths;
+	//private List<float> lengths;
 	
 
 	// Called when the node enters the scene tree for the first time.
@@ -28,11 +28,10 @@ public partial class PlayerTrail : Line2D
 		curve = new Curve2D();
 		tailSegments = new List<CollisionShape2D>();
 		timeActive = new List<float>();
-		lengths = new List<float>();
+		//lengths = new List<float>();
 		Modulate = playerRef.Modulate;
 		collisionShape.Shape = new SegmentShape2D();
 		segment.CollisionLayer = 2;
-		parent = GetParent<Area2D>() as Player;
 		callable = new Callable(this, nameof(OnAreaEntered));
 		
 	}
@@ -40,15 +39,17 @@ public partial class PlayerTrail : Line2D
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		// TODO: Additional possible changes: 1. Add int to delete a value every few frames regardless of time to prevent still trail when player stops moving
+		// TODO: 2. Make collisions only be on ~80% of the line to define hit box as it thins out at the end 
 		
-		Points = curve.GetBakedPoints(); // TODO: will this add points? no callable function yet
+		Points = curve.GetBakedPoints(); 
 
 		// Add new point
-		curve.AddPoint(new Vector2(parent.Position.X + 20 * (Mathf.Cos(parent.Rotation) * -1), 
-			parent.Position.Y + 20 * (Mathf.Sin(parent.Rotation) * -1)));
+		curve.AddPoint(new Vector2(playerRef.Position.X + 20 * (Mathf.Cos(playerRef.Rotation) * -1), 
+			playerRef.Position.Y + 20 * (Mathf.Sin(playerRef.Rotation) * -1)));
 		timeActive.Add(0); // new time tracker		
 		if(Points.Length > 1){
-			lengths.Add(Points.Last<Vector2>().DistanceTo(Points[Points.Length - 1]));
+			//lengths.Add(Points.Last<Vector2>().DistanceTo(Points[Points.Length - 1]));
 			Area2D newSegment = (Area2D)segment.Duplicate();
 			CollisionShape2D newColShape = (CollisionShape2D)collisionShape.Duplicate();
 			Shape2D newShape = (Shape2D)collisionShape.Shape.Duplicate();
@@ -76,12 +77,12 @@ public partial class PlayerTrail : Line2D
 		}
 		
 		// Time Culling
-		float timeAllowed = 1.0f / (parent.Health + 1);
+		float timeAllowed = 1.0f / (playerRef.Health + 1);
 		while(timeActive[0] > timeAllowed)
 		{
 			curve.RemovePoint(0);
 			timeActive.RemoveAt(0);
-			lengths.RemoveAt(0);
+			//lengths.RemoveAt(0);
 			var deleteThis = tailSegments[0];
 			tailSegments.RemoveAt(0);
 			deleteThis.QueueFree();
