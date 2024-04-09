695d12e72acf4e816d5d168875e5b12f2f954d3a
Donovan Scullion (RIT Student) <dps5393@rit.edu>
2024-04-09 13:46:03 -0400

    Got Timer working

---
+++ Project/assets/Scripts/TimeTracker.cs
@@ -0,0 +1,28 @@
+using Godot;
+using System;
+
+public partial class TimeTracker : Label
+{
+    private GameManager manager;
+
+    public override void _Ready()
+    {
+        manager = GetTree().Root.GetNode<GameManager>("Game2");
+    }
+    
+    public override void _Process(double delta)
+    { 
+        float seconds = (float)manager.WorldBorder.countDown.TimeLeft % 60;
+
+        if (manager.WorldBorder.countDown.TimeLeft > 59.99)
+        {
+            float minutes = (int)(manager.WorldBorder.countDown.TimeLeft / 60) % 60;
+
+            Text = $"{minutes}:{(int)seconds}";
+        }
+        else
+        {
+            Text = $"{seconds:F}";
+        }
+    }
+}
