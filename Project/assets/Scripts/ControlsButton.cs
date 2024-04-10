216f6427c8fe44c86fe7a38f6f3aabbe0365e008
Patrick Doherty <prd9410@rit.edu>
2024-04-10 00:26:47 -0400

    Fixed Controls Button, made it no longer an export

--- Project/assets/Scripts/ControlsButton.cs
+++ Project/assets/Scripts/ControlsButton.cs
@@ -3,11 +3,11 @@ using System;
 
 public partial class ControlsButton : Button
 {
-	[Export]
 	public Control _controlPop;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		_controlPop = GetNode<Control>("/root/StartMenu/Controls");
 	}
 
 	private void _on_pressed()
